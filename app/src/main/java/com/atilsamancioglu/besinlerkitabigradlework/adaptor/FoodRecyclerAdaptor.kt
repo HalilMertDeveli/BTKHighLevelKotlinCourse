17dc43bbc65312b08ca580de1f61364dc041deba
ted <alone_coder@gmial.com>
2023-01-30 00:24:30 +0300

    we wrote the adaptor for refleshing data

--- app/src/main/java/com/atilsamancioglu/besinlerkitabigradlework/adaptor/FoodRecyclerAdaptor.kt
+++ app/src/main/java/com/atilsamancioglu/besinlerkitabigradlework/adaptor/FoodRecyclerAdaptor.kt
@@ -6,8 +6,35 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.atilsamancioglu.besinlerkitabigradlework.R
 import com.atilsamancioglu.besinlerkitabigradlework.model.Food
-import kotlinx.android.synthetic.main.fragment_food_list.view.*
+import kotlinx.android.synthetic.main.food_recycler_row.view.*
+import java.util.zip.Inflater
 
-class FoodRecyclerAdaptor {
 
+class FoodRecyclerAdaptor(val foodList : ArrayList<Food>) : RecyclerView.Adapter<FoodRecyclerAdaptor.FoodViewHolder>() {
+    class FoodViewHolder (itemView:View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.food_recycler_row,parent,false)
+        return FoodViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return foodList.size
+    }
+
+    override fun onBindViewHolder(holder: FoodViewHolder, position: Int) {
+        holder.itemView.foodNameInFoodList.text = foodList[position].foodName //recycler row 'lar da ki field lar
+        holder.itemView.foodCalorieInFoodList.text = foodList[position].foodCalorie
+        //Image  view will be add
+    }
+
+    //we wrote this function for refreshing  food list data
+    fun updateFoodList ( newFoodList : List<Food>) {
+        foodList.clear()
+        foodList.addAll(newFoodList)
+        notifyDataSetChanged()
+    }
 }
