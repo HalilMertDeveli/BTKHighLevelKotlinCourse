8364150b85806eb4291bd442d09672e9d90e3293
ted <alone_coder@gmial.com>
2023-01-29 03:29:04 +0300

    we wrote only FoodListFragment Navigation Code

--- app/src/main/java/com/atilsamancioglu/besinlerkitabigradlework/FoodListFragment.kt
+++ app/src/main/java/com/atilsamancioglu/besinlerkitabigradlework/FoodListFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_food_list.*
 
 
 class FoodListFragment : Fragment() {
@@ -21,6 +23,16 @@ class FoodListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_food_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        foodListToFoodDetailButton.setOnClickListener {
+            val root = FoodListFragmentDirections.actionFoodListFragmentToFoodDetailFragment(3)
+            Navigation.findNavController(it).navigate(root)
+
+        }
+
+        super.onViewCreated(view, savedInstanceState)
     }
 
 
