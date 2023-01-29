53633b3dc9c204263e83a05a3f4d86506ca26624
ted <alone_coder@gmial.com>
2023-01-29 06:16:08 +0300

    we added the adaptor but didnt write

---
+++ app/src/main/java/com/atilsamancioglu/besinlerkitabigradlework/adaptor/FoodRecyclerAdaptor.kt
@@ -0,0 +1,13 @@
+package com.atilsamancioglu.besinlerkitabigradlework.adaptor
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.atilsamancioglu.besinlerkitabigradlework.R
+import com.atilsamancioglu.besinlerkitabigradlework.model.Food
+import kotlinx.android.synthetic.main.fragment_food_list.view.*
+
+class FoodRecyclerAdaptor {
+
+}
