16b4412713c7e95958ea8abb561f427d835200d8
ted <alone_coder@gmial.com>
2023-01-29 03:09:24 +0300

    we added two clean fragment

--- app/src/main/java/com/atilsamancioglu/besinlerkitabigradlework/FoodDetailFragment.kt
+++ app/src/main/java/com/atilsamancioglu/besinlerkitabigradlework/FoodDetailFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class FoodListFragment : Fragment() {
+class FoodDetailFragment : Fragment() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +20,7 @@ class FoodListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_list, container, false)
+        return inflater.inflate(R.layout.fragment_food_detail, container, false)
     }
 
 
