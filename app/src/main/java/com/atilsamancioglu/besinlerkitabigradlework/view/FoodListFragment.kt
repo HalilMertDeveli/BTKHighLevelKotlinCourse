8f07a294e4d48550b884652078297de12b68b889
Halil Mert <halilmertdeveliii@gmail.com>
2023-02-09 01:15:15 +0300

    we finished the Fragment ve ViewModel Lessons and continues Verileri Göstermek , min 02:00

--- app/src/main/java/com/atilsamancioglu/besinlerkitabigradlework/view/FoodListFragment.kt
+++ app/src/main/java/com/atilsamancioglu/besinlerkitabigradlework/view/FoodListFragment.kt
@@ -5,10 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.atilsamancioglu.besinlerkitabigradlework.R
+import com.atilsamancioglu.besinlerkitabigradlework.adaptor.FoodRecyclerAdaptor
+import com.atilsamancioglu.besinlerkitabigradlework.view_model.FoodListViewModel
+import kotlinx.android.synthetic.main.fragment_food_list.*
 
 
 class FoodListFragment : Fragment() {
+
+    private lateinit var viewModel : FoodListViewModel
+    private val recyclerFoodAdaptor = FoodRecyclerAdaptor(arrayListOf())
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,9 +33,16 @@ class FoodListFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
+        viewModel = ViewModelProviders.of(this).get(FoodListViewModel::class.java)
+        viewModel.refreshData()
+        foodListRecyclerView.layoutManager= LinearLayoutManager(context)
+        foodListRecyclerView.adapter= recyclerFoodAdaptor
 
         super.onViewCreated(view, savedInstanceState)
+    }
+    //we are using this method for watch Data
+    fun observeLiveData(){
+
     }
 
 
--- app/src/main/java/com/atilsamancioglu/besinlerkitabigradlework/view_model/FoodListViewModel.kt
+++ app/src/main/java/com/atilsamancioglu/besinlerkitabigradlework/view_model/FoodListViewModel.kt
@@ -4,9 +4,21 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.atilsamancioglu.besinlerkitabigradlework.model.Food
 
-class FoodListViewModel: ViewModel() {
+class FoodListViewModel : ViewModel() {
 
-    val foods = MutableLiveData<List<Food>>()
-    val foodErrorMessage = MutableLiveData<Boolean>()
-    val foodIsLooading = MutableLiveData<Boolean>()
+    private val foods = MutableLiveData<List<Food>>()
+    private val foodErrorMessage = MutableLiveData<Boolean>()
+    private val foodIsLoading = MutableLiveData<Boolean>()
+
+    fun refreshData() {
+        val banana = Food("Banana", "12", "15", "10", "2", "www.test.com")
+        val strawberry = Food("strawberry", "200", "21", "22", "2", "www.test.com")
+        val cucumber = Food("cucumber", "300", "30", "31", "3", "www.test.com")
+
+        val foodArrayListOf = arrayListOf<Food>(banana, strawberry, cucumber)
+
+        foods.value = foodArrayListOf
+        foodErrorMessage.value = false
+        foodIsLoading.value = false
+    }
 }
